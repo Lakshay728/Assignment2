fa476c8b0fa4c1e11d96bad423477c9f992d854f
Lakshay728 <lakshaymudgil18102005@gmail.com>
2025-10-23 20:22:36 +0530

    add the code

--- hey.py
+++ hey.py
@@ -1,1 +1,16 @@
-print("hello")
+# Task 1
+'''num = int(input("Enter a number : "))
+
+if num % 2 == 0:
+    print(num,"is a Even number")
+else:
+    print(num,"is a Odd number")'''
+
+
+# Task 2
+sum = 0
+
+for i in range (1 , 51):
+    sum +=i
+
+print("The sum of the numbers from 1 to 50 is:",sum)
